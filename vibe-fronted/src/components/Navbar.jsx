6c5c68a4c338d7d3dd1ce495916dfce3760f200a
Marcos <padilladortamarcosalejandro@gmail.com>
2023-02-01 00:34:21 -0500

    Changed Navbar design

--- vibe-fronted/src/components/Navbar.jsx
+++ vibe-fronted/src/components/Navbar.jsx
@@ -1,36 +1,165 @@
 import { useState } from 'react'
+import { styled, alpha } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import AccountCircle from '@mui/icons-material/AccountCircle'
+import Typography from '@mui/material/Typography'
+import InputBase from '@mui/material/InputBase'
+import Badge from '@mui/material/Badge'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import SearchIcon from '@mui/icons-material/Search'
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import MailIcon from '@mui/icons-material/Mail'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import MoreIcon from '@mui/icons-material/MoreVert'
 import { useAuthContext } from '../context/AuthProvider'
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginRight: theme.spacing(2),
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(5),
+		width: 'auto',
+	},
+}))
 
-export default function MenuAppBar() {
-	const { logout, user } = useAuthContext()
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}))
 
-	const [auth, setAuth] = useState(true)
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: '20ch',
+		},
+	},
+}))
+
+export default function Navbar() {
 	const [anchorEl, setAnchorEl] = useState(null)
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null)
+	const { user, logout } = useAuthContext()
+	const isMenuOpen = Boolean(anchorEl)
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
 
-	const handleChange = (event) => {
-		setAuth(event.target.checked)
-	}
-
-	const handleMenu = (event) => {
+	const handleProfileMenuOpen = (event) => {
 		setAnchorEl(event.currentTarget)
 	}
 
-	const handleClose = () => {
-		setAnchorEl(null)
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null)
 	}
 
-	const handleLogout = () => {
-		logout()
+	const handleMenuClose = () => {
+		setAnchorEl(null)
+		handleMobileMenuClose()
 	}
+
+	const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget)
+	}
+
+	const menuId = 'primary-search-account-menu'
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={menuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			<MenuItem onClick={handleMenuClose}>{user.user.name}</MenuItem>
+			<MenuItem
+				onClick={() => {
+					handleMenuClose()
+					logout()
+				}}
+			>
+				Logout
+			</MenuItem>
+		</Menu>
+	)
+
+	const mobileMenuId = 'primary-search-account-menu-mobile'
+	const renderMobileMenu = (
+		<Menu
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}
+		>
+			<MenuItem>
+				<IconButton size='large' aria-label='show 4 new mails' color='inherit'>
+					<Badge badgeContent={4} color='error'>
+						<MailIcon />
+					</Badge>
+				</IconButton>
+				<p>Messages</p>
+			</MenuItem>
+			<MenuItem>
+				<IconButton
+					size='large'
+					aria-label='show 17 new notifications'
+					color='inherit'
+				>
+					<Badge badgeContent={17} color='error'>
+						<NotificationsIcon />
+					</Badge>
+				</IconButton>
+				<p>Notifications</p>
+			</MenuItem>
+			<MenuItem onClick={handleProfileMenuOpen}>
+				<IconButton
+					size='large'
+					aria-label='account of current user'
+					aria-controls='primary-search-account-menu'
+					aria-haspopup='true'
+					color='inherit'
+				>
+					<AccountCircle />
+				</IconButton>
+				<p>Profile</p>
+			</MenuItem>
+		</Menu>
+	)
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
@@ -40,53 +169,77 @@ export default function MenuAppBar() {
 						size='large'
 						edge='start'
 						color='inherit'
-						aria-label='menu'
+						aria-label='open drawer'
 						sx={{ mr: 2 }}
 					>
 						<MenuIcon />
 					</IconButton>
 					<Typography
-						variant='h4'
+						variant='h6'
+						noWrap
 						component='div'
-						sx={{ flexGrow: 1 }}
-						style={{
-							letterSpacing: 5,
-						}}
+						sx={{ display: { xs: 'none', sm: 'block' } }}
+						letterSpacing={5}
 					>
 						VIBE
 					</Typography>
-					<div>
+					<Search>
+						<SearchIconWrapper>
+							<SearchIcon />
+						</SearchIconWrapper>
+						<StyledInputBase
+							placeholder='Search…'
+							inputProps={{ 'aria-label': 'search' }}
+						/>
+					</Search>
+					<Box sx={{ flexGrow: 1 }} />
+					<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
 						<IconButton
 							size='large'
+							aria-label='show 4 new mails'
+							color='inherit'
+						>
+							<Badge badgeContent={4} color='error'>
+								<MailIcon />
+							</Badge>
+						</IconButton>
+						<IconButton
+							size='large'
+							aria-label='show 17 new notifications'
+							color='inherit'
+						>
+							<Badge badgeContent={17} color='error'>
+								<NotificationsIcon />
+							</Badge>
+						</IconButton>
+						<IconButton
+							size='large'
+							edge='end'
 							aria-label='account of current user'
-							aria-controls='menu-appbar'
+							aria-controls={menuId}
 							aria-haspopup='true'
-							onClick={handleMenu}
+							onClick={handleProfileMenuOpen}
 							color='inherit'
 						>
 							<AccountCircle />
 						</IconButton>
-						<Menu
-							id='menu-appbar'
-							anchorEl={anchorEl}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorEl)}
-							onClose={handleClose}
+					</Box>
+					<Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+						<IconButton
+							size='large'
+							aria-label='show more'
+							aria-controls={mobileMenuId}
+							aria-haspopup='true'
+							onClick={handleMobileMenuOpen}
+							color='inherit'
 						>
-							<MenuItem>{user.user.name}</MenuItem>
-							<MenuItem onClick={handleLogout}>Logout</MenuItem>
-						</Menu>
-					</div>
+							<MoreIcon />
+						</IconButton>
+					</Box>
 				</Toolbar>
 			</AppBar>
+			{renderMobileMenu}
+			{renderMenu}
 		</Box>
 	)
 }
