e5a5f5af39fa383a92f30ec04bb6fabe6b235854
Marcos <padilladortamarcosalejandro@gmail.com>
2023-02-01 21:38:18 -0500

    Added sidebar items

--- vibe-fronted/src/components/Sidebar.jsx
+++ vibe-fronted/src/components/Sidebar.jsx
@@ -3,10 +3,46 @@ import { useAuthContext } from '../context/AuthProvider'
 import LogoutIcon from '@mui/icons-material/Logout'
 import SettingsIcon from '@mui/icons-material/Settings'
 import { IconButton } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import { Button } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { Link } from 'react-router-dom'
+
+const SidebarButton = styled(Button)({
+	border: 'none',
+	width: '100%',
+	fontSize: 30,
+	color: 'white',
+	borderBottom: '1px solid lightgray',
+	borderRadius: 0,
+	'&:hover': {
+		outlined: 'none',
+		border: 'none',
+		background: 'rgb(18,85,152)',
+	},
+})
+
+const SidebarItem = ({ name, icon, link }) => {
+	return (
+		<Link to={link} style={{ textDecoration: 'none' }}>
+			<SidebarButton variant='outlined' startIcon={icon} color='secondary'>
+				{name}
+			</SidebarButton>
+		</Link>
+	)
+}
+
+const sidebarItems = [
+	{
+		name: 'Home',
+		icon: <HomeIcon />,
+		link: '/',
+	},
+]
 
 export default function Sidebar({ status }) {
 	const sidebarClassName = `sidebar ${!status ? 'hide' : ''}`
-	const { user } = useAuthContext()
+	const { user, logout } = useAuthContext()
 
 	return (
 		<div className={sidebarClassName}>
@@ -30,13 +66,24 @@ export default function Sidebar({ status }) {
 							style={{ color: 'lightgray', fontSize: 40 }}
 						/>
 					</IconButton>
-					<IconButton aria-label='settings' size='large'>
+					<IconButton
+						aria-label='settings'
+						size='large'
+						onClick={() => {
+							logout()
+						}}
+					>
 						<LogoutIcon
 							fontSize='inherit'
 							style={{ color: 'lightgray', fontSize: 40 }}
 						/>
 					</IconButton>
 				</div>
+			</div>
+			<div className='items'>
+				{sidebarItems.map((item) => (
+					<SidebarItem key={item.name} {...item} />
+				))}
 			</div>
 		</div>
 	)
